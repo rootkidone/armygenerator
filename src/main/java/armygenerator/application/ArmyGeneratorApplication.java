200d9662592b95e2f5f12811478cd0e4ab5cb9aa
frank columbo <columbo.coding@googlemail.com>
2019-07-02 19:27:31 +0200

    gg-ag: make application spring boot runnable

--- src/main/java/armygenerator/application/ArmyGeneratorApplication.java
+++ src/main/java/armygenerator/application/ArmyGeneratorApplication.java
@@ -1,5 +1,11 @@
 package armygenerator.application;
 
-public class ArmyGeneratorApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class ArmyGeneratorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArmyGeneratorApplication.class, args);
+	}
 }
