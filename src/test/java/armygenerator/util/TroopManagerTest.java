0aefa8513e9d1aa2014bf196fab3a86efd0ed1ef
frank columbo <columbo.coding@googlemail.com>
2019-07-03 00:54:30 +0200

    gg-ag: add test for TroopManager

---
+++ src/test/java/armygenerator/util/TroopManagerTest.java
@@ -0,0 +1,48 @@
+package armygenerator.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import armygenerator.troops.TroopEnum;
+
+public class TroopManagerTest {
+	
+	private static TroopManagerObject manager;
+
+	
+	@BeforeClass
+	public static void setup() {
+		manager = new TroopManagerObject();
+	}
+	
+	@Test
+	public void test_add_troop_archers() {
+		int numberOfArchers = 5;
+		for(int i = 0; i < numberOfArchers; i++) {			 
+			manager.add(TroopEnum.ARCHER);
+		}
+		Integer archers = manager.getTroopManagementMap().get(TroopEnum.ARCHER);
+		Assertions.assertThat(archers).isEqualTo(numberOfArchers);
+	}
+	
+	@Test
+	public void test_add_troop_spearmen() {
+		int numberOfSpearman = 5;
+		for(int i = 0; i < numberOfSpearman; i++) {			 
+			manager.add(TroopEnum.SPEARMAN);
+		}
+		Integer archers = manager.getTroopManagementMap().get(TroopEnum.SPEARMAN);
+		Assertions.assertThat(archers).isEqualTo(numberOfSpearman);
+	}
+	
+	@Test
+	public void test_add_troop_swordsman() {
+		int numberOfSwordmen = 5;
+		for(int i = 0; i < numberOfSwordmen; i++) {			 
+			manager.add(TroopEnum.SWORDMAN);
+		}
+		Integer archers = manager.getTroopManagementMap().get(TroopEnum.SWORDMAN);
+		Assertions.assertThat(archers).isEqualTo(numberOfSwordmen);
+	}
+}
